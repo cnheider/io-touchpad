fab629bf37cd39f483bcbed4e37939c399af9375
Mateusz Piotrowski <mpp302@gmail.com>
2016-04-04 03:01:50 +0200

    Add comments and clean up the code. Other changes:
    - Change tabs to spaces.
    - Remove some old code.

--- touchpadlib/touchpadlib.h
+++ touchpadlib/touchpadlib.h
@@ -5,23 +5,26 @@
 #include <linux/input.h>
 
 struct touchpad_event {
-    int32_t x;
-    int32_t y;
-    int32_t pressure;
-    time_t seconds;
-    suseconds_t useconds;
+    int32_t     x;          // The absolute coordinate x.
+    int32_t     y;          // The absolute coordinate y.
+    int32_t     pressure;   // Pressure applied to touchpad surface.
+    time_t      seconds;    // Timestamp of the event occurance in seconds.
+    suseconds_t useconds;   // Additional precision for in miliseconds.
 };
 
-int has_root_privileges(void); // @TODO
-int initalize_touchpadlib_usage();
-int fetch_touchpad_event(int fd, struct touchpad_event *touchpad_event);
-void print_event(const struct touchpad_event *event);
-struct touchpad_event *new_event();
+/* struct touchpad_event interface. */
 void erase_event(struct touchpad_event *event);
 int get_x(struct touchpad_event *event);
 int get_y(struct touchpad_event *event);
 int get_pressure(struct touchpad_event *event);
 int get_seconds(struct touchpad_event *event);
 int get_useconds(struct touchpad_event *event);
+struct touchpad_event *new_event(void);
+
+/* touchpadlib interface */
+int fetch_touchpad_event(int fd, struct touchpad_event *event);
+int has_root_privileges(void);
+int initalize_touchpadlib_usage(void);
+void print_event(const struct touchpad_event *event);
 
 #endif /* TOUCHPADLIB_H */
