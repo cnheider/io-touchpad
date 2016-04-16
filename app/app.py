00ba956ec74fbbccc8364e502452711f9501231c
Mateusz Piotrowski <mpp302@gmail.com>
2016-04-16 14:47:16 +0200

    Cleaned up app.py.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" io-touchpad application.
+"""io-touchpad application.
 
 This application will learn to associate symbols you draw on your touchpad
 with certain actions you bound to those symbols.
@@ -15,19 +15,24 @@ Source code encoding: UTF-8
 import queue
 
 from terminationhandler import terminationhandler
-from touchpadsignal import touchpadsignal
 from signalcollection import signalcollection
 from threads import application
 from threads import listener
 
-# SIGINT signal handler.
-terminationhandler.setup()
 
-# Global variables.
-queue = queue.Queue()
-collection = signalcollection.SignalCollection()
+def main():
+    """The main function."""
+    # SIGINT signal handler.
+    terminationhandler.setup()
 
-# Run both threads.
-print("\nUse your touchpad as usual. Have a nice day!")
-listener.start(queue)
-application.application_thread(collection, queue)
+    # Global variables.
+    thread_queue = queue.Queue()
+    collection = signalcollection.SignalCollection()
+
+    # Run both threads.
+    print("\nUse your touchpad as usual. Have a nice day!")
+
+    listener.start(thread_queue)
+    application.application_thread(collection, thread_queue)
+
+main()
