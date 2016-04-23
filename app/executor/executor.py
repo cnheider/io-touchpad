cf7fb81e49b1db6dee23c8b8bcc93941931fc2f0
Mateusz Piotrowski <mpp302@gmail.com>
2016-04-23 13:31:34 +0200

    Change the name of data module to databox.
    data might be used in the future to store static and generated files.
    The change purpose is to avoid the name conflict in the future.

--- app/executor/executor.py
+++ app/executor/executor.py
@@ -6,28 +6,28 @@ This module executes commands.
 """
 
 import subprocess
-from data import data
+from databox import databox
 
 
 def execute(command_id):
     """Execute the command related to the command_id.
 
     This function executes the command it can match with command_id using
-    the data module.
+    the databox module.
 
     If the command is found then a child process will be started. It does not
     wait for its termination.
 
     Args:
-        command_id: The id which will be matched with a command from the data
-            module. This argument will be casted to a string.
+        command_id: The id which will be matched with a command from
+            the databox module. This argument will be casted to a string.
 
     Returns:
         True if the related command was found and it tried to launch it. False
         otherwise.
     """
     command_id = str(command_id)
-    command, arguments = data.get_command_and_arguments(command_id)
+    command, arguments = databox.get_command_and_arguments(command_id)
     if command is None:
         print("ERROR: executor: The command related to the detected symbol "
               "is missing.")
