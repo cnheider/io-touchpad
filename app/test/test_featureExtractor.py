6cc9a3b809c467ac517171defd90b7b610a4b1e4
rjiukyagami <emil.hotkowski@gmail.com>
2016-04-25 17:16:29 +0200

    Cleaning

--- app/test/test_featureExtractor.py
+++ app/test/test_featureExtractor.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 
-"""Tests for signal collection """
+"""Tests for signal collection"""
 
 import pytest
 
 from classifier import featureExtractor
 
-#Basic points
-pointA = [2.0,4.0]
-pointB = [1.0,2.0]
+# Basic points
+POINTA = [2.0, 4.0]
+POINTB = [1.0, 2.0]
 
 def test_center_of_line():
-    """Simple center of line test"""
-    assert featureExtractor.center_of_line(pointA, pointB) == (1.5, 3.0)
+    """Simple center of line test
+    
+    """
+    assert featureExtractor.center_of_line(POINTA, POINTB) == (1.5, 3.0)
 
 def test_length_of_line():
-    """Simple length of line test"""
-    assert featureExtractor.length_of_line(pointA, pointB) == 2.23606797749979
+    """Simple length of line test
 
-#Class for mocking signal
+    """
+    assert featureExtractor.length_of_line(POINTA, POINTB) == 2.23606797749979
+
+# Class for mocking signal
 class Signal_test:
     
     def __init__(self, x, y):
@@ -31,20 +35,26 @@ class Signal_test:
     def get_y(self):
         return self.y
 
-signalA = Signal_test(1.0, 1.0)
-signalB = Signal_test(2.0, 2.0)
-signal_list_test = [signalA, signalB]
+SIGNALA = Signal_test(1.0, 1.0)
+SIGNALB = Signal_test(2.0, 2.0)
+SIGNAL_LIST_TEST = [SIGNALA, SIGNALB]
 
 def test_calculate_border_points():
-    """Test the border point of signal test list"""
-    assert featureExtractor.calculate_border_points(signal_list_test) == (1.0,1.0,2.0,2.0)
+    """Test the border point of signal test list
+
+    """
+    assert featureExtractor.calculate_border_points(SIGNAL_LIST_TEST) == (1.0,1.0,2.0,2.0)
 
 def test_calculate_center_of_mass_and_length():
-    """Test for calculate center of mass and length"""
-    assert featureExtractor.calculate_center_of_mass_and_length(signal_list_test) == ((1.5, 1.5), 1.4142135623730951)#TODO: check with data
+    """Test for calculate center of mass and length
+
+    """
+    assert featureExtractor.calculate_center_of_mass_and_length(SIGNAL_LIST_TEST) == ((1.5, 1.5), 1.4142135623730951)#TODO: check with data
 
 def test_ratio_point_of_line():
-    """Test for ratio point of line function"""
-    assert featureExtractor.ratio_point_of_line(pointA, pointB, 0.5) == (1.5, 3.0)
+    """Test for ratio point of line function
+    
+    """
+    assert featureExtractor.ratio_point_of_line(POINTA, POINTB, 0.5) == (1.5, 3.0)
 
 
