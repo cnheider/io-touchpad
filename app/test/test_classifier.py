e93a05c6fd1338ac50b5497fdc24622deff69871
rjiukyagami <emil.hotkowski@gmail.com>
2016-04-25 19:24:07 +0200

    Little cleaning

--- app/test/test_classifier.py
+++ app/test/test_classifier.py
@@ -3,11 +3,10 @@
 """Tests for classifier"""
 
 import pytest
-import sys
 
 from classifier import classifier
 
-cifier = classifier.Classifier(True,None)
+cifier = classifier.Classifier(True, None)
 
 def test_reset_training_set():
     """Test for load training set 
@@ -25,11 +24,11 @@ def test_compute_tolerance_distance():
     we put some list of list of features to calculate 
     fixed distance, and check if it's same
     """
-    L1 = [11.2, 41.43 , 1.33]
-    L2 = [10.9, 41.45 , 1.34]
-    L3 = [12.0, 41.4412 , 1.001]
-    L4 = [11.3, 41.15 , 1.12]
-    L5 = [11.223, 41.0 , 1.31]
+    L1 = [11.2, 41.43, 1.33]
+    L2 = [10.9, 41.45, 1.34]
+    L3 = [12.0, 41.4412, 1.001]
+    L4 = [11.3, 41.15, 1.12]
+    L5 = [11.223, 41.0, 1.31]
     AL = [L1, L2, L3, L4, L5]
     cifier.compute_tolerance_distance(AL)
 
