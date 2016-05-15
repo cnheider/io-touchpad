34d13e5cad61f89fa44a6261d1727119b49f7b3a
Mateusz Piotrowski <mpp302@gmail.com>
2016-05-15 21:43:54 +0200

    Refactor command line arguments.
    The structure of the arguments changed drastically.
    
    I've got to test it if I broke it or not.

--- app/app.py
+++ app/app.py
@@ -19,38 +19,53 @@ import sys
 from terminationhandler import terminationhandler
 from threads import application
 from threads import listener
-from classifier import classifier
+from classifier import classifier as classifier_module
 
 MIN_TRAINING_SIZE = 3
 
+RUN_SUBCOMMAND = 'run'
+RUN_USER_MODE = 'user'
+RUN_32_MODE = '32'
+RUN_64_MODE = '64'
+LEARN_SUBCOMMAND = 'learn'
+REPEAT_SUBCOMMAND = 'repeat'
 
 def _get_configured_parser():
     """Configure the commandline arguments parser."""
     description = 'Teaching your touchpad magic tricks since 2016.'
     parser = argparse.ArgumentParser(add_help=True, description=description)
 
-    group = parser.add_mutually_exclusive_group()
+    subparsers = parser.add_subparsers(metavar='SUBCOMMAND', dest='subcommand',
+            help='available subcommands; run <subcommand> -h to show help for '
+            'the <subcommand>')
+    subparsers.required = True # Require SUBCOMMAND to be specified.
 
-    group.add_argument('-b', '--bitness', dest='system_bitness',
-                       default=None, metavar='BITS',
-                       help='set the bitness of your operation system; '
-                       'this option triggers the use of the hardcoded '
-                       'symbols', choices={'32', '64'})
-    group.add_argument('-l', '--learning', dest='training_size',
-                       default=None, metavar='SIZE',
-                       help='start the application in the learning mode; '
-                       'you will be asked to draw a symbol SIZE times; '
-                       'SIZE should be at least ' + str(MIN_TRAINING_SIZE),
-                       type=int)
-    group.add_argument('-r', '--repeat', dest='repeat_classification',
-                       default=False, action='store_true',
-                       help='repeat the classification on the latest '
-                       'user-defined set of drawings')
+    parser_run = subparsers.add_parser(RUN_SUBCOMMAND, help='a subcommand to '
+            'run the app using hardcoded or user-defined symbols')
+    parser_run.add_argument(dest='run_mode', metavar='MODE',
+            choices={RUN_32_MODE, RUN_64_MODE, RUN_USER_MODE},
+            help='set the mode you would like to run the app in; use <32> '
+            'for 32-bit machines, <64> for 64-bit machines and <user> to use '
+            'user-defined symbols')
 
-    group2 = parser.add_mutually_exclusive_group()
-    group2.add_argument('-s', '--symbol', dest='symbol_name',
-                       default=None, metavar='NAME',
-                       help='name of symbol to learn in the learning mode')
+    parser_learn = subparsers.add_parser(LEARN_SUBCOMMAND,
+            help='a subcommand to undertake a learning session')
+    parser_learn.add_argument(dest='training_size', default=None,
+            metavar='SIZE', help='you will be asked to draw a symbol SIZE '
+            'times; SIZE should be at least ' + str(MIN_TRAINING_SIZE),
+            type=int)
+    parser_learn.add_argument(dest='symbol_name', default=None,
+            metavar='SYMBOL', help='the name of the symbol ' 'you want the '
+            'app to learn during the learning session')
+
+    parser_repeat = subparsers.add_parser(REPEAT_SUBCOMMAND,
+            help='a subcommand to repeat the classification process of all '
+            'symbols; use the --symbol option to repeat the process for '
+            'only one symbol.')
+    parser_repeat.add_argument('-s', '--symbol', dest='symbol_name',
+            default=None, metavar='SYMBOL', help='the name of the symbol on '
+            'which you want to repeat the classification process')
+
     return parser
 
 
@@ -62,26 +77,33 @@ def main():
     parser = _get_configured_parser()
     args = parser.parse_args()
 
-    training_size = args.training_size
-    learning_mode = training_size is not None
-    symbol_name = args.symbol_name
+    training_size = None
+    system_bitness = None
+    learning_mode = None
+    symbol_name = None
 
-    if args.repeat_classification:
+    #  if args.repeat_classification:
+    if args.subcommand == REPEAT_SUBCOMMAND:
         print('Repeating the classification within the learning process.')
-        clsf = classifier.Classifier(True)
-        clsf.learn(True, symbol_name)
+        classifier = classifier_module.Classifier(True)
+        classifier.learn(True, args.symbol_name)
         sys.exit(0)
 
-    if learning_mode:
-        print(args.symbol_name)
+    #  if learning_mode:
+    if args.subcommand == LEARN_SUBCOMMAND:
+        print('The symbol name is ' + args.symbol_name + '.')
         if training_size < 3:
             print('app.py: error: the training size should be at least %d'
                   % (MIN_TRAINING_SIZE), file=sys.stderr)
             sys.exit(1)
+        training = args.training_size
+        symbol_name = args.symbol_name
+        learning_mode = True
 
-    system_bitness = args.system_bitness
-    if system_bitness is not None:
-        system_bitness = int(system_bitness)
+    if args.subcommand == RUN_SUBCOMMAND:
+        if args.run_mode != RUN_USER_MODE:
+            system_bitness = int(args.run_mode)
+
 
     thread_queue = queue.Queue()
 
