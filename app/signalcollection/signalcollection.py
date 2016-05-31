35bb4723e63bbef0b08b442302f1e73e5a7cc14e
Mateusz Piotrowski <mpp302@gmail.com>
2016-05-31 23:40:52 +0200

    Improve performance.
    I removed the problem with active waiting in the application.py.
    I used threading.Condition to set up a communication channal so that
    the application thread can wait if there is nothing to do and the
    listener thread can wake it up.
    
    Relates #42.

--- app/signalcollection/signalcollection.py
+++ app/signalcollection/signalcollection.py
@@ -2,7 +2,7 @@
 
 """A collection of signals.
 
-Signal is an item taken out of the queue. It might be something more than a
+A signal is an item taken out of the queue. It might be something more than a
 touchpad event.
 """
 
@@ -39,12 +39,12 @@ class SignalCollection:
         Args:
             signal (TouchpadSignal): A new signal to be added.
         """
-        while self.need_to_remove_head(signal):
+        while self._need_to_remove_head(signal):
             self.signal_list.pop(0)
         self.signal_list.append(signal)
 
     def is_recent_enough(self, current_time):
-        """Check if the last signal recent enough.
+        """Check if the last signal is recent enough.
 
         Compares the time of the occurance of the tail signal with the
         current time.
@@ -52,48 +52,94 @@ class SignalCollection:
         Args:
             current_time (float): The current time which is compared with
                 the time of the last signal in the list.
+
+        Retruns:
+            True if the (current_time - tail_time) is less or equal
+            MAX_BREAK_BETWEEN_TWO_SIGNALS. True is returned if the signal_list
+            is empty. False otherwise.
         """
-        if self.is_empty():
-            return True
-        tail_time = self.signal_list[-1].get_time()
-        return current_time - tail_time <= MAX_BREAK_BETWEEN_TWO_SIGNALS
+        try:
+            tail_time = self.signal_list[-1].get_time()
+        except IndexError:
+            result = True
+        else:
+            result = current_time - tail_time <= MAX_BREAK_BETWEEN_TWO_SIGNALS
+        return result
+
+    def get_time_when_old_enough(self, current_time):
+        """Get the amount of time left for the tail to get old.
+
+        The tail is the tail of the signal_list.
+
+        Args:
+            current_time (float): The current time which is compared with
+                the time of the last signal in the list.
+
+        Returns:
+            The difference between the current time and the time of the tail.
+            None is returned if the signal_list is empty.
+        """
+        try:
+            tail_time = self.signal_list[-1].get_time()
+        except IndexError:
+            result_time = None
+        else:
+            result_time = current_time - tail_time
+        return result_time
 
     def as_list(self):
-        """Return the SignalCollection as a list."""
+        """Return the SignalCollection as a list.
+
+        Returns:
+            The signal_list as a list.
+        """
         return self.signal_list
 
-    def need_to_remove_head(self, signal):
+    def _need_to_remove_head(self, signal):
         """Check if there is a need of the list's head removal.
-
-        Return true if the list is too big or if the head is too old.
 
         Args:
             signal (TouchpadSignal): A signal. Needed to calculate if the head
                 is too old or not.
+
+        Returns:
+            True if the list is too big or if the head is too old.
+            False otherwise.
         """
-        if self.is_empty():
+        if self._is_empty():
             return False
-        elif self.is_too_big():
+        elif self._is_too_big():
             return True
-        elif self.is_head_too_old(signal):
+        elif self._is_head_too_old(signal):
             return True
         else:
             return False
 
-    def is_empty(self):
-        """True if signal list is empty."""
+    def _is_empty(self):
+        """Check if the signal_list is empty.
+
+        Returns:
+            True if signal list is empty. False otherwise.
+        """
         return self.signal_list == []
 
-    def is_too_big(self):
-        """True if signal list is too big."""
+    def _is_too_big(self):
+        """Check if the signal_list is not too big.
+
+        Returns:
+            True if signal list is too big. False otheriwse.
+        """
         return len(self.signal_list) >= MAX_NUMBER_OF_SIGNALS_IN_GROUP
 
-    def is_head_too_old(self, signal):
-        """True if the head of signal list is too old.
+    def _is_head_too_old(self, signal):
+        """Check if the head of the signal_list is too old.
 
         Args:
             signal (TouchpadSignal): A signal which is compared with the head
                 in terms of the time attribute.
+
+        Returns:
+            True if the head of signal list is too old.
         """
         signal_time = signal.get_time()
         head_time = self.signal_list[0].get_time()
--- app/threads/listener.py
+++ app/threads/listener.py
@@ -12,24 +12,32 @@ import _thread
 from touchpadsignal import touchpadsignal
 
 
-def start(queue):
+def start(queue, condition):
     """Start the listener thread.
 
     Args:
-        queue, (Queue): An inter-thread communication queue for threads.
+        queue (Queue): An inter-thread communication queue for threads.
+        condition (Condition): A condition which allows the threads to notify
+            each other and wait if there is nothing to do.
+
     """
-    _thread.start_new_thread(listener_thread, (queue,))
+    _thread.start_new_thread(listener_thread, (queue, condition))
 
 
-def listener_thread(queue):
+def listener_thread(queue, condition):
     """The main function of the listener thread.
 
     The listener thread fetches events from the touchpad and pushes them onto
     the queue.
 
     Args:
-        queue, (Queue): An inter-thread communication queue for threads.
+        queue (Queue): An inter-thread communication queue for threads.
+        condition (Condition): A condition which allows the threads to notify
+            each other and wait if there is nothing to do.
     """
     while True:
         touchpad_signal = touchpadsignal.TouchpadSignal()
         queue.put(touchpad_signal, True)
+        condition.acquire()
+        condition.notify()
+        condition.release()
